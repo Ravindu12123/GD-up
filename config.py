079afb5bce027be0f700c658cc5c395814c72fae
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2024-12-05 18:42:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    AUTHORIZED_USERS = list(map(int, os.getenv("AUTHORIZED_USERS").split(',')))
-    GDRIVE_CREDENTIALS = os.getenv("GDRIVE_CREDENTIALS")  # Path to `credentials.json`
+    BOT_TOKEN = os.getenv("tk")
+    API_ID = int(os.getenv("apiid"))
+    API_HASH = os.getenv("apihash")
+    AUTHORIZED_USERS = list(map(int, os.getenv("auth").split(',')))
+    GDRIVE_CREDENTIALS = os.getenv("gdcred","credentials.json")  # Path to `credentials.json`
     UPLOAD_FOLDER = "uploads/"
